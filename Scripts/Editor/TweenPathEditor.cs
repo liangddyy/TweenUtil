12fe103472f0cfa1c6747f3acb9a161e0dedb8d2
liangddyy <liangddyy@foxmail.com>
2018-04-20 18:46:21 +0800

    更新~

---
+++ Scripts/Editor/TweenPathEditor.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEditor;
+
+namespace Tween
+{
+	[CustomEditor(typeof(TweenPath))]
+	public class TweenPathEditor : Editor
+	{
+		private TweenPath tweenPath;
+		private void OnEnable()
+		{
+			tweenPath = target as TweenPath;
+		}
+
+		private void OnDisable()
+		{ 
+
+		}
+
+		public override void OnInspectorGUI()
+		{
+			GUILayout.BeginVertical();
+			for (int i = 0; i < tweenPath.Data.Nodes.Count; i++)
+			{
+				EditorGUILayout.Vector3Field("",tweenPath.Data.Nodes[i]);
+			}
+			
+			GUILayout.EndVertical();
+		}
+
+		private void OnSceneGUI()
+		{
+			throw new System.NotImplementedException();
+		}
+	}
+}
